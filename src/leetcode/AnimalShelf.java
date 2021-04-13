d578e898c02f9d1848c08bd9a933276a84f4564a
bubblexin <360023721@qq.com>
2021-04-13 09:41:58 +0800

    add 1. 二叉搜索树第 k 大节点 \n 2. LRU 缓存机制 \n 3. 猫狗队列

---
+++ src/leetcode/AnimalShelf.java
@@ -0,0 +1,96 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 面试题 03.06. 动物收容所（猫狗队列）
+ * https://leetcode-cn.com/problems/animal-shelter-lcci/
+ * https://52heartz.top/articles/cat-dog-queue/  用类实现
+ *
+ * <p>
+ * 动物收容所。有家动物收容所只收容狗与猫，且严格遵守“先进先出”的原则。在收养该收容所的动物时，收养人只能收养所有动物中“最老”（由其进入收容所的时间长短而定）的动物，或者可以挑选猫或狗（同时必须收养此类动物中“最老”的）。换言之，收养人不能自由挑选想收养的对象。请创建适用于这个系统的数据结构，实现各种操作方法，比如enqueue、dequeueAny、dequeueDog和dequeueCat。允许使用Java内置的LinkedList数据结构。
+ * <p>
+ * enqueue方法有一个animal参数，animal[0]代表动物编号，animal[1]代表动物种类，其中 0 代表猫，1 代表狗。
+ * <p>
+ * dequeue*方法返回一个列表[动物编号, 动物种类]，若没有可以收养的动物，则返回[-1,-1]。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入：
+ * ["AnimalShelf", "enqueue", "enqueue", "dequeueCat", "dequeueDog", "dequeueAny"]
+ * [[], [[0, 0]], [[1, 0]], [], [], []]
+ * 输出：
+ * [null,null,null,[0,0],[-1,-1],[1,0]]
+ * 示例2:
+ * <p>
+ * 输入：
+ * ["AnimalShelf", "enqueue", "enqueue", "enqueue", "dequeueDog", "dequeueCat", "dequeueAny"]
+ * [[], [[0, 0]], [[1, 0]], [[2, 1]], [], [], []]
+ * 输出：
+ * [null,null,null,null,[2,1],[0,0],[1,0]]
+ * 说明:
+ * <p>
+ * 收纳所的最大容量为 20000
+ */
+public class AnimalShelf {
+    public Queue<int[]> dequeDog;
+    public Queue<int[]> dequeCat;
+    public static final int CAT = 0;
+    public static final int DOG = 1;
+
+    public AnimalShelf() {
+        dequeDog = new LinkedList<>();
+        dequeCat = new LinkedList<>();
+    }
+
+    /**
+     * animal[0]代表动物编号，animal[1]代表动物种类
+     * 其中 0 代表猫，1 代表狗。
+     *
+     * @param animal
+     */
+    public void enqueue(int[] animal) {
+        if (animal[1] == CAT) {
+            dequeCat.offer(animal);
+        } else {
+            dequeDog.offer(animal);
+        }
+    }
+
+    public int[] dequeueAny() {
+        if (dequeCat.isEmpty() && !dequeDog.isEmpty()) {
+            // 猫空，狗不空，返回狗头
+            return dequeDog.poll();
+        } else if (!dequeCat.isEmpty() && dequeDog.isEmpty()) {
+            // 狗空，猫不空，返回猫头
+            return dequeCat.poll();
+        } else if (dequeCat.isEmpty() && dequeDog.isEmpty()) {
+            // 猫狗都空，返回 [-1,-1]
+            return new int[]{-1, -1};
+        } else {
+            // 猫狗都不空，比较两个队列头，谁在前面返回谁
+            int[] cat = dequeCat.peek();
+            int[] dog = dequeDog.peek();
+            if (cat[0] < dog[0]) {
+                return dequeCat.poll();
+            } else {
+                return dequeDog.poll();
+            }
+        }
+    }
+
+    public int[] dequeueDog() {
+        if (!dequeDog.isEmpty()) {
+            return dequeDog.poll();
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int[] dequeueCat() {
+        if (!dequeCat.isEmpty()) {
+            return dequeCat.poll();
+        }
+        return new int[]{-1, -1};
+    }
+}
---
+++ src/leetcode/offer/Offer54.java
@@ -0,0 +1,56 @@
+package leetcode.offer;
+
+import leetcode.bean.TreeNode;
+
+/**
+ * 剑指 Offer 54. 二叉搜索树的第k大节点
+ * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+ * <p>
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [3,1,4,null,2], k = 1
+ * 3
+ * / \
+ * 1   4
+ * \
+ * 2
+ * 输出: 4
+ * 示例 2:
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * 5
+ * / \
+ * 3   6
+ * / \
+ * 2   4
+ * /
+ * 1
+ * 输出: 4
+ * <p>
+ * 限制：
+ * <p>
+ * 1 ≤ k ≤ 二叉搜索树元素个数
+ */
+public class Offer54 {
+    int res, k;
+
+    public int kthLargest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    /**
+     * 由二叉搜索树的定义可知，按照中序遍历的倒序遍历，遍历 k 次，即是第 k 大的数
+     * @param root
+     */
+    public void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.right);
+        if (k == 0) return;
+        if (--k == 0) res = root.val;
+        dfs(root.left);
+    }
+}
