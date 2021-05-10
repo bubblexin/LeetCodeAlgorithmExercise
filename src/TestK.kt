93473b66a2f2728bcf0f2673916e8bda0696d239
bubblexin <360023721@qq.com>
2021-05-10 08:34:53 +0800

    add TopK

--- src/TestK.kt
+++ src/TestK.kt
@@ -1,2 +1,14 @@
-class TestK {
+class TestK<out Fruit>:Any() {
+    private var fruit: Fruit? = null
+    fun getString(): String {
+        return ""
+    }
+
+    fun getS(): Fruit? {
+        return fruit
+    }
+
+    fun getSList(): List<Fruit> {
+        return arrayListOf()
+    }
 }
